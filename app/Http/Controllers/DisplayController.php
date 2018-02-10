52927738d65d605b5af9577d8b2427b09ae9c08f
Raarujeed <mustafaraar@gmail.com>
2018-02-10 15:18:18 +0300

    extracted data from sample xml file

--- app/Http/Controllers/DisplayController.php
+++ app/Http/Controllers/DisplayController.php
@@ -34,6 +34,31 @@ class DisplayController extends Controller {
     }
 
     private function processUpload($file) {
-        dd($file);
+        // get path name
+        $filePathName = simplexml_load_file($file->getPathName());
+        foreach($filePathName->HOTELS as $keyHotels => $valueHotel){
+    		foreach($valueHotel as $data => $rooms){
+		    	$hotelData = new Hotel();
+				$hotelData->hotel_name = $rooms->attributes()->HOTEL_NAME;
+				$hotelData->hotel_location = $rooms->attributes()->LOCATION;
+				$hotelData->hotel_rating = $rooms->attributes()->RATING;
+				$hotelData->hotel_available = $rooms->attributes()->AVAILABLE;
+				$hotelData->hotel_currency = $rooms->attributes()->CURRENCY;
+				$hotelData->hotel_starting_price = $rooms->attributes()->STARTING_PRICE;
+				$hotelData->hotel_isRecommended = $rooms->attributes()->ISRECOMMENDEDPRODUCT;
+				$hotelData->save();
+	    		foreach($rooms as $keyRoom => $valueRoom) {
+	    			foreach($valueRoom as $data1 => $room) {
+				    	$roomData = new Room();
+						$roomData->room_name = $room->ROOM_NAME;
+						$roomData->room_occupancy = $room->OCCUPANCY;
+						$roomData->room_status = $room->ROOM_STATUS;
+						$roomData->hotel_id = $hotelData->id;
+						$roomData->save();
+	    			}
+	    		}
+    		}
+        }
+        return redirect('/');
     }
 }
